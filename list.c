8d24563d7c15e36169918b34cf3ccd50e51d43a0
Replit user <replituser@example.com>
2021-03-25 22:15:23 +0000

    new try

--- list.c
+++ list.c
@@ -81,9 +81,7 @@ void pushFront(List * list,  const void * data) {
   n->data = data;
   n->next = (void *) data;
 
-  list->head = n;
-
-
+  list->tail = n;
 }
 
 void pushBack(List * list, const void * data) {
