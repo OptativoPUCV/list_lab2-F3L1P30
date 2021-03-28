30c88d768f3a5b5b840d79747ce940ffe682553e
Replit user <replituser@example.com>
2021-03-28 02:27:19 +0000

    new try

--- list.c
+++ list.c
@@ -109,7 +109,7 @@ void * popCurrent(List * list) {
   
   
   
-  Node *n = createNode(list->current);
+  Node *n = list->current;
   //n->prev = list->current->prev;
   //n->next = list->current->next;
    void * data = (int *)list->current->data;
